a62d3eab4b9bbd91da07698786c9fae2108c853d
Sydney Pearce <spearc3@students.towson.edu>
2021-12-09 22:55:51 -0500

    gradle files for drew

---
+++ app/src/main/java/com/example/leftovers/RestApplication.kt
@@ -0,0 +1,4 @@
+package com.example.leftovers
+
+class RestApplication {
+}
---
+++ app/src/main/java/com/example/leftovers/di/TestAppModule.kt
@@ -0,0 +1,32 @@
+package com.example.leftovers.di
+
+import android.app.Application
+import com.example.leftovers.data.IBankRepository
+import com.example.leftovers.data.api.BankApiComm
+import com.example.leftovers.data.impl.BanksDatabaseRepository
+import com.example.leftovers.network.FoodFetcher
+import com.example.leftovers.network.IFoodFetcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class FoodModule {
+
+    @Provides
+    fun provideFoodsRepository(
+        db: BanksDatabaseRepository,
+        foodsFetcher: IFoodFetcher
+    ): IBankRepository {
+        return BankApiComm(db, foodsFetcher)
+    }
+
+    @Provides
+    fun provideFoodsFetcher(
+        app: Application
+    ): IFoodFetcher {
+        return FoodFetcher(app)
+    }
+}
