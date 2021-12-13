90008b40dec973d4e5352424552a7057f30a413d
Sydney Pearce <spearc3@students.towson.edu>
2021-12-13 13:55:35 -0500

    more unit tests

--- app/src/test/java/com/example/leftovers/LoginUnitTest.kt
+++ app/src/test/java/com/example/leftovers/LoginUnitTest.kt
@@ -1,56 +1,39 @@
 package com.example.leftovers
 
-<<<<<<< HEAD
-import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.leftovers.ui.landingpage.LandingPageViewModel
-=======
 import android.app.Application
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.test.performTextInput
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import androidx.test.internal.runner.junit3.AndroidJUnit3Builder
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.leftovers.ui.homescreen.HomeScreenView
 import com.example.leftovers.ui.landingpage.LandingPageViewModel
-import com.example.leftovers.ui.nav.Routes
-import com.google.errorprone.annotations.DoNotMock
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import junit.framework.Assert.assertEquals
->>>>>>> 974b003661bd4e2c8c26b8f6567905ba07d39285
 import junit.framework.Assert.assertTrue
 import org.junit.Assert
-import org.junit.Rule
+
+
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.junit.MockitoJUnit
-import org.mockito.runners.MockitoJUnitRunner
+
+import org.junit.Rule
 
 
-
-<<<<<<< HEAD
-
-class LoginUnitTest {
-//    @get:Rule
-//    val hiltRule = HiltAndroidRule(this)
-=======
+@RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
 class LoginUnitTest {
 
     private lateinit var mockApp: Application
 
     private lateinit var mockNav: NavHostController
+
     @get:Rule
     val hiltRule = HiltAndroidRule(this)
 
@@ -76,27 +59,21 @@ class LoginUnitTest {
         Assert.assertEquals(1, vm.userMode.value)
     }
 
-
->>>>>>> 974b003661bd4e2c8c26b8f6567905ba07d39285
-//
-//    @get:Rule
-//    val composeTestRule = createAndroidComposeRule<MainActivity>()
-
     @Composable
     @Test
     fun loginButtonTest() {
-//        composeTestRule.mainClock.autoAdvance = false
-//
-//        composeTestRule.setContent {
-            //arrange
-            val vm: LandingPageViewModel = viewModel()
-            Assert.assertNull(vm.userMode.value)
+        composeTestRule.mainClock.autoAdvance = false
 
-            //act
-            vm.setUserMode(1)
-            //assert
-            assertTrue("Restaurant", true)
-        //}
+        composeTestRule.setContent {
+        //arrange
+        val vm: LandingPageViewModel = viewModel()
+        Assert.assertNull(vm.userMode.value)
+
+        //act
+        vm.setUserMode(1)
+        //assert
+        assertTrue("Restaurant", true)
+        }
     }
 }
 
